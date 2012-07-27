9ff7e817e5e4d5c8236113b8b9b0ed7e84ed23d5
caseywatts <casey.s.watts@gmail.com>
2012-07-27 12:33:24 -0400

    tooltips appear on question mark tds

--- servicenowtweaks.js
+++ servicenowtweaks.js
@@ -12,10 +12,10 @@
       { "labelname": "u_it_business_service", "labeltitle": "STC uses 'Personal Productivity' as default. Most common selections:\n\nPersonal Productivity\nEliApps\nEmail - Legacy" },
       { "labelname": "u_incident_type",       "labeltitle": "STC uses 'Service Interruption' as default.\nThe others should be used during more widespread problems than we deal with in the TTO." },
       { "labelname": "u_it_provider_service", "labeltitle": "STC uses 'Personal Computing' as default. Most common selections ones:\n\nPersonal Computing\nSoftware Library\nTelecom" },
-      { "labelname": "u_level_1",             "labeltitle": "Most common selections for STC:\nHardware => Personal Computer => Laptop\nHardware => Personal Computer => Desktop\nHardware => Data Storage => Storage Device\nHardware => Mobile => Phone\nHardware => Mobile => Tablet\n\nHardware => Network => Router/Switch\nHardware => Network => VPN\nHardware => Network => WiFi\n\nHardware => Audio Visual => (many options)\n\nApplication Software => Client" },
-      { "labelname": "u_level_2",             "labeltitle": "Most common selections for STC:\nHardware => Personal Computer => Laptop\nHardware => Personal Computer => Desktop\nHardware => Data Storage => Storage Device\nHardware => Mobile => Phone\nHardware => Mobile => Tablet\n\nHardware => Network => Router/Switch\nHardware => Network => VPN\nHardware => Network => WiFi\n\nHardware => Audio Visual => (many options)\n\nApplication Software => Client" },
-      { "labelname": "u_level_3",             "labeltitle": "Most common selections for STC:\nHardware => Personal Computer => Laptop\nHardware => Personal Computer => Desktop\nHardware => Data Storage => Storage Device\nHardware => Mobile => Phone\nHardware => Mobile => Tablet\n\nHardware => Network => Router/Switch\nHardware => Network => VPN\nHardware => Network => WiFi\n\nHardware => Audio Visual => (many options)\n\nApplication Software => Client" },
-      { "labelname": "u_asset_device",        "labeltitle": "Ask LAL" },
+      { "labelname": "u_level_1",             "labeltitle": "Most common selections for STC:\nHardware => Personal Computer => Laptop\nHardware => Personal Computer => Desktop\nHardware => Data Storage => Storage Device\nHardware => Mobile => Phone\nHardware => Mobile => Tablet\n\nHardware => Audio Visual => (many options)\n\nHardware => Network => Router/Switch\nHardware => Network => VPN\nHardware => Network => WiFi\n\nNetwork => Environment => Ethernet\nNetwork => Environment => WiFi\n\nApplication Software => Client\n" },
+      { "labelname": "u_level_2",             "labeltitle": "Most common selections for STC:\nHardware => Personal Computer => Laptop\nHardware => Personal Computer => Desktop\nHardware => Data Storage => Storage Device\nHardware => Mobile => Phone\nHardware => Mobile => Tablet\n\nHardware => Audio Visual => (many options)\n\nHardware => Network => Router/Switch\nHardware => Network => VPN\nHardware => Network => WiFi\n\nNetwork => Environment => Ethernet\nNetwork => Environment => WiFi\n\nApplication Software => Client\n" },
+      { "labelname": "u_level_3",             "labeltitle": "Most common selections for STC:\nHardware => Personal Computer => Laptop\nHardware => Personal Computer => Desktop\nHardware => Data Storage => Storage Device\nHardware => Mobile => Phone\nHardware => Mobile => Tablet\n\nHardware => Audio Visual => (many options)\n\nHardware => Network => Router/Switch\nHardware => Network => VPN\nHardware => Network => WiFi\n\nNetwork => Environment => Ethernet\nNetwork => Environment => WiFi\n\nApplication Software => Client\n" },
+      { "labelname": "u_asset_device",        "labeltitle": "Ask ST Managers" },
       { "labelname": "u_component",           "labeltitle": "Specific software or type of device affected.\n(hardware components aren't recorded in SN)\nMost common other ones:\n\nAcrobat Pro X\nAdobe Reader\n\nApple Mail\nThunderbird\nMicrosoft Outlook 2003\nMicrosoft Outlook 2007\nMicrosoft Outlook 2010\nMicrosoft Outlook 2011\n\nEmail Alias\nNetID - Student/ID Interfaces\nNetID/Pin/Password\n\nExcel 2010\n\nDropbox\n\niPad\niPhone\n\nWindows 7\n\nIE 8\nIE7\n\nWiFi\nYale Secure\nYaleGuest\n\nAnyConnect Mobility Client (VPN)\nVPN\n\nYale Secure File Transfer" },
       { "labelname": "opened_at",             "labeltitle": "" },
       { "labelname": "opened_by",             "labeltitle": "" },
@@ -37,7 +37,11 @@
 function taglabels(){
   for (var i = 0; i < labeltags.length; i++) {
     labeltag = jQuery("#label\\.incident\\." + labeltags[i]['labelname'])
-    labeltag.attr('title', labeltags[i]['labeltitle'])
+    labeltag.after('<td style="background-color:#DDD; vertical-align:middle; padding-left:5px; padding-right:5px;" title="' + labeltags[i]['labeltitle'] +  '"> ? </td>')
+    //labeltag.parent().children(':last-child').append('<span style="padding-left:5px;" title="' + labeltags[i]['labeltitle'] +  '">(?)</span>')
+    //.append('<span style="padding-left:5px;">(?)</div>')
+    //labeltag.append('<span style="float:right;" title="' + labeltags[i]['labeltitle'] +  '">(?)</span>')
+    //labeltag.attr('title', labeltags[i]['labeltitle'])
   }
 }
 
